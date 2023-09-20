0dd71ea7b18b638987a81cf379010c23613a14cc
fahimahammed <fahimahammed.cse@gmail.com>
2023-09-20 15:37:13 +0600

    configs

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express, { Application } from 'express';
+import httpStatus from 'http-status';
+import globalExceptionHandler from './app/middlewares/globalExceptionHandler';
+import routes from './app/routes';
 
 const app: Application = express();
 
@@ -9,8 +12,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-  res.json({ message: "server started" })
+app.use('/api/v1', routes);
+
+app.use(globalExceptionHandler);
+
+app.use((req, res, next) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: 'API not found',
+    errorMessages: [
+      {
+        path: '',
+        message: 'API not found'
+      }
+    ]
+  });
 });
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,45 @@
 import { Server } from 'http';
 import app from './app';
+import subScribeToEvents from './app/events';
+import config from './config';
+import logger from './shared/logger';
+import { RedisClient } from './shared/redis';
 
 async function bootstrap() {
-
-  const server: Server = app.listen(3000, () => {
-    console.log("server listening")
+  await RedisClient.connect().then(() => {
+    subScribeToEvents();
   });
 
+  const server: Server = app.listen(config.port, () => {
+    logger.info(`Server running on port ${config.port}`);
+  });
+
+  const exitHandler = () => {
+    RedisClient.unsubscribe();
+    RedisClient.disconnect();
+
+    if (server) {
+      server.close(() => {
+        logger.info('Server closed');
+      });
+    }
+    process.exit(1);
+  };
+
+  const unexpectedErrorHandler = (error: unknown) => {
+    logger.error(error);
+    exitHandler();
+  };
+
+  process.on('uncaughtException', unexpectedErrorHandler);
+  process.on('unhandledRejection', unexpectedErrorHandler);
+
+  process.on('SIGTERM', () => {
+    logger.info('SIGTERM received');
+    if (server) {
+      server.close();
+    }
+  });
 }
 
 bootstrap();
