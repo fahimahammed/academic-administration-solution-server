bbaa90e9983a4b47f5a1c08ccbeb48fbf0ac9cfa
fahimahammed <fahimahammed.cse@gmail.com>
2024-06-19 20:56:05 +0600

    updated:

---
+++ src/app/modules/semesterRegistration/semesterRegistration.test.ts
@@ -0,0 +1,47 @@
+import config from '../../../config';
+import { testGetApi } from '../../../shared/testUtils';
+
+let Id: string = "testId";
+const baseApi: string = `${config.test.baseApi}/semester-registrations`;
+
+describe('Semester Registration Management', () => {
+
+    testGetApi(
+        'should retrive all semester registration',
+        baseApi,
+        200,
+        true,
+        "SemesterRegistrations fetched successfully",
+        true
+    );
+
+    testGetApi(
+        'should retrive my semester registration with access token',
+        `${baseApi}/my-registration`,
+        200,
+        true,
+        "Successfully fetched my registration",
+        true,
+        config.test.studentToken
+    );
+
+    // testGetApi(
+    //     'should retrive my semester registration courses with access token',
+    //     `${baseApi}/my-semester-registration-courses`,
+    //     200,
+    //     true,
+    //     "Successfully fetched my registration courses",
+    //     true,
+    //     config.test.studentToken
+    // );
+
+    testGetApi(
+        'should retrive semester registration with id',
+        `${baseApi}/${Id}`,
+        200,
+        true,
+        "SemesterRegistration fetched successfully",
+        true
+    );
+
+});
