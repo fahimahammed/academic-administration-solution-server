cdf84c56005abad54ce8b85d45e73e178d0cbf3d
fahimahammed <fahimahammed.cse@gmail.com>
2024-06-19 21:11:09 +0600

    :tada: student course mark test

---
+++ src/app/modules/studentEnrolledCourseMark/studentEnrolledCourseMark.test.ts
@@ -0,0 +1,29 @@
+import config from '../../../config';
+import { testGetApi } from '../../../shared/testUtils';
+
+let Id: string = "testId";
+const baseApi: string = `${config.test.baseApi}/student-enrolled-course-marks`;
+
+describe('Student enrolled course mark Management', () => {
+
+    testGetApi(
+        'should retrive all student enrolled course marks',
+        baseApi,
+        200,
+        true,
+        "Student course marks fetched successfully",
+        true,
+        config.test.facultyToken
+    );
+
+    testGetApi(
+        'should retrive student enrolled course with id',
+        `${baseApi}/my-marks`,
+        200,
+        true,
+        "Student course marks fetched successfully",
+        true,
+        config.test.studentToken
+    );
+
+});
