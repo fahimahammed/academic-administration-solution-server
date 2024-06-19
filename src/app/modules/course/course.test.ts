99ebc5f938e78688c2433080906862537f594806
fahimahammed <fahimahammed.cse@gmail.com>
2024-06-19 13:58:56 +0600

    :tada: add course test

---
+++ src/app/modules/course/course.test.ts
@@ -0,0 +1,26 @@
+import config from '../../../config';
+import { testGetApi } from '../../../shared/testUtils';
+
+const Id: string = "438272525-kshfs-2342";
+const baseApi: string = `${config.testBase}/courses`;
+
+describe('Course Management', () => {
+    testGetApi(
+        'should retrive all course',
+        baseApi,
+        200,
+        true,
+        "Courses fetched successfully",
+        true
+    );
+
+    testGetApi(
+        'should retrive course with id',
+        `${baseApi}/${Id}`,
+        200,
+        true,
+        "Course fetched successfully",
+        true
+    )
+
+});
