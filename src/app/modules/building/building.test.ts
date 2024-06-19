1c90f65cd7a893a2f8e2f29e12236d3efe913d48
fahimahammed <fahimahammed.cse@gmail.com>
2024-06-19 13:56:14 +0600

    :bulb: add test for buildings

---
+++ src/app/modules/building/building.test.ts
@@ -0,0 +1,26 @@
+import config from '../../../config';
+import { testGetApi } from '../../../shared/testUtils';
+
+const buildingId: string = "438272525-kshfs-2342";
+const baseApi: string = `${config.testBase}/buildings`;
+
+describe('Building Management', () => {
+    testGetApi(
+        'should retrive all building',
+        baseApi,
+        200,
+        true,
+        "Buildings fetched successfully",
+        true
+    );
+
+    testGetApi(
+        'should retrive building with id',
+        `${baseApi}/${buildingId}`,
+        200,
+        true,
+        "Building fetched successfully",
+        true
+    )
+
+});
