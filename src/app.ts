d565d6ee93991d768eb64c23bb1f337f4eecaaeb
fahimahammed <fahimahammed.cse@gmail.com>
2024-09-17 13:16:28 +0600

    updated

--- src/app.ts
+++ src/app.ts
@@ -14,8 +14,10 @@ const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // Limit each IP to 100 requests per windowMs
   message: {
-    success: false,
-    message: 'Too many requests from this IP, please try again after 15 minutes.',
+    data: {
+      success: false,
+      message: 'Too many requests from this IP, please try again after 15 minutes.',
+    }
   },
 });
 
