27f3de134319bc5aedcbde0bdc08c60801f4b059
fahimahammed <fahimahammed.cse@gmail.com>
2024-10-25 02:01:47 +0600

    updated

--- src/app.ts
+++ src/app.ts
@@ -23,15 +23,16 @@ const limiter = rateLimit({
 
 app.use(limiter); // Apply the rate limiter to all requests
 
-app.use(cors({
-  credentials: true,
-  origin: "http://localhost:3000"
-}));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
 SeedDB.seedSuperAdmin(); // seed super admin
+
+app.use(cors({
+  credentials: true,
+  origin: "http://localhost:3000"
+}));
 
 app.use('/api/v1', routes);
 
